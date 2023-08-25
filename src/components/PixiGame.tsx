0661f80f82e1dbcf1f541fa4587d21497f8b2db3
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-25 18:49:18 +0400

    Added score text

--- src/components/PixiGame.tsx
+++ src/components/PixiGame.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import * as PIXI from 'pixi.js';
+import { Application, Sprite, Text } from 'pixi.js';
 import { root } from '../main.tsx';
 import { movePlayer, incrementScore, selectPlayerState } from '../features';
 
@@ -9,19 +9,19 @@ function PixiGame() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const app = new PIXI.Application({ width: 800, height: 500 });
+    const app = new Application({ width: 800, height: 500 });
 
     if (root) {
       root.appendChild(app.view as HTMLCanvasElement);
     }
 
-    const player = PIXI.Sprite.from('../images/player.png');
+    const player = Sprite.from('../images/player.png');
     player.anchor.set(0.5);
     player.x = app.view.width / 2;
     player.y = app.view.height / 2 - 100;
     app.stage.addChild(player);
 
-    const item = PIXI.Sprite.from('../images/item.png');
+    const item = Sprite.from('../images/item.png');
     item.x = app.view.width / 2;
     item.y = app.view.height / 2 + 100;
     item.width = 100;
@@ -29,6 +29,16 @@ function PixiGame() {
     item.interactive = true;
     item.on('click', () => dispatch(incrementScore()));
     app.stage.addChild(item);
+
+    const scoreText = new Text('Score 0', {
+      fontFamily: 'Arial',
+      fontWeight: 'bold',
+      fontSize: 24,
+      fill: 'white',
+    });
+    scoreText.x = 15;
+    scoreText.y = 10;
+    app.stage.addChild(scoreText);
 
     const handleKeyDown = (event: KeyboardEvent) => {
       const speed = 5;
