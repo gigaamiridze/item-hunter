be79a65675fe9283254ba2f4e0ca472899166aba
Giga Amiridze <gigaamiridze@gmail.com>
2023-08-25 17:24:26 +0400

    Added player image using Sprite

--- src/components/PixiGame.tsx
+++ src/components/PixiGame.tsx
@@ -9,6 +9,12 @@ function PixiGame() {
     if (root) {
       root.appendChild(app.view as HTMLCanvasElement);
     }
+
+    const player = PIXI.Sprite.from('../images/player.png');
+    player.anchor.set(0.5);
+    player.x = app.view.width / 2;
+    player.y = app.view.height / 2 - 100;
+    app.stage.addChild(player);
   }, []);
 
   return null;
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Provider } from 'react-redux';
 import store from './store';
-import '../src/assets/styles/reset.css';
-import '../src/assets/styles/index.css';
+import './styles/reset.css';
+import './styles/index.css';
 
 export const root = document.getElementById('root');
 
